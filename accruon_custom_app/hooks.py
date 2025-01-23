27caa90204d8e20dc169d72ae48914c956137b9a
Josin Benny <josinbenny@gmail.com>
2025-01-23 11:14:29 +0530

    remove message

--- accruon_custom_app/hooks.py
+++ accruon_custom_app/hooks.py
@@ -26,7 +26,7 @@ app_license = "mit"
 
 # include js, css files in header of desk.html
 # app_include_css = "/assets/accruon_custom_app/css/accruon_custom_app.css"
-app_include_js = "/assets/accruon_custom_app/js/accruon_custom_app.js"
+# app_include_js = "/assets/accruon_custom_app/js/accruon_custom_app.js"
 
 # include js, css files in header of web template
 # web_include_css = "/assets/accruon_custom_app/css/accruon_custom_app.css"
